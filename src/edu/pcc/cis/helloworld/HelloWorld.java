fa6f12bbed807eef3cc2a2ce11c3457b04e2757c
noah <noah.taylor@pcc.edu>
2018-02-06 22:02:04 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
 /**
  * Created by Noah Taylor on 2018.02.06
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
